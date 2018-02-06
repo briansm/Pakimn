9c52226d11bcfd5df41767f3de0cc0d6c0b6b192
Brian Morales <briansmor09@gmail.com>
2018-02-05 21:29:44 -0600

    Moviendo clase a otro archivo .js

--- paki.js
+++ paki.js
@@ -3,28 +3,7 @@ var imagenes=[];
 imagenes["Cauchin"]="vaca.png";
 imagenes["Pokacho"]="pollo.png";
 imagenes["Tocinauro"]="cerdo.png";
-class Pakiman{
-  constructor(n,v,a){
-    this.imagen=new Image();
-    this.nombre=n;
-    this.vida=v;
-    this.ataque=a;
 
-    this.imagen.src=imagenes[this.nombre];
-  }
-  //Dentro de la clase no es necesario colocar 'function'
-  hablar(){
-    alert(this.nombre);
-  }
-
-  mostrar(){
-    document.body.appendChild(this.imagen);
-    document.write("<br/><strong>"+this.nombre+"</strong><br/>");
-    document.write("Vida: "+this.vida+"<br/>");
-    document.write("Ataque: "+this.ataque+"<hr/>");
-
-  }
-}
 
 var cauchin=new Pakiman("Cauchin",100,30);
 var pokacho=new Pakiman("Pokacho",80,50);
