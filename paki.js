22bb6964fdb3fea7886d1a4c1af673c48714c107
Brian Morales <briansmor09@gmail.com>
2018-02-05 20:58:34 -0600

    Creando objetos

--- paki.js
+++ paki.js
@@ -3,9 +3,13 @@ class Pakiman{
     this.nombre=n;
     this.vida=v;
     this.ataque=a;
-    this.tipo="Tierra";
+  }
+  //Dentro de la clase no es necesario colocar 'function'
+  hablar(){
+    alert(this.nombre);
   }
 }
 
-var cauchin=new Pakiman();
-var pokacho=new Pakiman();
+var cauchin=new Pakiman("Cauchin",100,30);
+var pokacho=new Pakiman("Pokacho",80,50);
+var tocinauro=new Pakiman("Tocinauro",120,40);
