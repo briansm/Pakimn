0a1051ffab341924be7ce4745765a7cf231ae251
Brian Morales <briansmor09@gmail.com>
2018-02-05 20:45:23 -0600

    Creando clase pakiman

--- paki.js
+++ paki.js
@@ -1,1 +1,11 @@
-alert("Atrapalos ya");
+class Pakiman{
+  constructor(n,v,a){
+    this.nombre=n;
+    this.vida=v;
+    this.ataque=a;
+    this.tipo="Tierra";
+  }
+}
+
+var cauchin=new Pakiman();
+var pokacho=new Pakiman();
